f818215320fb66cf59fd77e514f277844fde8436
Madhianandhan <152016565+Madhianandhan06@users.noreply.github.com>
2026-09-11 20:13:44 +0530

    feat: added login form

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,25 @@ const App = () => {
       <button onClick={() => setCount(c => c-1)}>-</button>
       <h3>{count}</h3>
       <button onClick={() => setCount(c => c+1)}>+</button>
+
+      <div>
+        <form action="">
+          <div>
+              <label htmlFor="">Enter Your Name:</label>
+              <input type="text" />
+          </div>
+
+          <div>
+              <label htmlFor="">Enter Your Email:</label>
+              <input type="text" />
+          </div>
+
+          <div>
+              <label htmlFor="">Enter Your Password:</label>
+              <input type="text" />
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
